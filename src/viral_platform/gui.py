43c01f73c9270d025526e6988d255b3ee4bcaf07
John Hardin <84035484+BeyondOurMinds@users.noreply.github.com>
2026-05-19 02:24:39 +0100

    getting basic Dash program setup

--- src/viral_platform/gui.py
+++ src/viral_platform/gui.py
@@ -1,5 +1,10 @@
-class app:
-    def __init__(self):
-        pass
+from viral_platform.app import create_app
+from viral_platform.layout import create_layout
 
-    
+class ViralApp:
+    def __init__(self):
+        self.app = create_app()
+        self.app.layout = create_layout()
+
+    def run(self):
+        self.app.run(debug=True)
---
+++ src/viral_platform/layout.py
@@ -0,0 +1,4 @@
+from dash import html
+
+def create_layout():
+    return html.Div("Hello, Viral Platform!")
